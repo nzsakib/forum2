7ac01484a30be4900f6e4e4145884f2e08215e26
Nazmus Sakib <sukku.mia@gmail.com>
2018-12-09 20:16:52 +0600

    Episode 6: A User May Publish Threads

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ Route::get('/', function () {
 });
 
 Route::get('/threads', 'ThreadsController@index');
+Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::post('/threads/{thread}/replies', 'RepliesController@store'); 
+
