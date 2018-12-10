4408ff3b1960eba1da26704b6d937b61b44f3e9c
Nazmus Sakib <sukku.mia@gmail.com>
2018-12-10 09:05:29 +0600

    Episode 7 : Let's Make Some Testing Helpers

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -14,8 +14,8 @@ class CreateThreadsTest extends TestCase
 	public function guest_may_not_create_threads()
 	{
 		$this->expectException('Illuminate\Auth\AuthenticationException');
-		
-		$thread = factory('App\Thread')->make();
+
+		$thread = make('App\Thread');
 
 		$this->post('/threads', $thread->toArray());
 	}
@@ -23,13 +23,13 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
-    	$this->actingAs(factory('App\User')->create());
+    	$this->signIn();
 
-    	$thread = factory('App\Thread')->make();
+    	$thread = make('App\Thread');
 
-    	$this->post('/threads', $thread->toArray());
+    	$response = $this->post('/threads', $thread->toArray());
 
-    	$this->get($thread->path())
+    	$this->get($response->getTargetUrl())
 				->assertSee($thread->title)
     			->assertSee($thread->body);
     }
