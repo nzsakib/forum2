46e0514c280a4b249ac4670df846448b124b2f24
Nazmus Sakib <sukku.mia@gmail.com>
2018-12-11 07:36:50 +0600

    Episode 10:  How to Test Validation Errors

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -23,14 +23,50 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
+        $this->withoutExceptionHandling();
     	$this->signIn();
 
-    	$thread = create('App\Thread');
+    	$thread = make('App\Thread');
 
     	$response = $this->post('/threads', $thread->toArray());
 
     	$this->get($response->getTargetUrl())
 				->assertSee($thread->title)
     			->assertSee($thread->body);
+    }
+
+    /** @test */
+    public function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+                ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+                ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+                ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+                ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides)
+    {
+        $this->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 }
