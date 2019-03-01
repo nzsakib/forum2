74a657c423628fffb8c3bd18921ffde98122e8fa
Nazmus Sakib <sukku.mia@gmail.com>
2019-03-01 14:44:49 +0600

    Episode 21: Global Scopes and Further Query Reduction

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -116,7 +116,7 @@ class ThreadsController extends Controller
 
     protected function getThreads(Channel $channel, ThreadFilters $filters) : Collection
     {
-        $threads = Thread::with('channel')->filter($filters)->latest();
+        $threads = Thread::filter($filters)->latest();
 
         if($channel->exists) {
             $threads->where('channel_id', $channel->id);
